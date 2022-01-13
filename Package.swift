27edbc68e07ab5287d7b2d087e023416590bc698
Bruno <brunochenchihying@gmai.com>
2022-01-13 14:31:16 -0300

    Update package file

--- Package.swift
+++ Package.swift
@@ -12,17 +12,13 @@ let package = Package(
             name: "pocjournys",
             targets: ["pocjournys"]),
     ],
-    dependencies: [
-        .package(path: "Home"),
-        .package(path: "Login"),
-        .package(path: "Profile")
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "pocjournys",
-            dependencies: ["Home", "Login", "Profile"]),
+            sources: ["Home", "Login", "Profile"]),
         .testTarget(
             name: "NetworkingInterfacesTests",
             dependencies: ["NetworkingInterfaces"]),
