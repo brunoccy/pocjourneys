a8178b805e0b6fb755e853bd3bf69f222f7c9eab
Bruno <brunochenchihying@gmai.com>
2022-01-13 18:04:36 -0300

    Fix package

--- Package.swift
+++ Package.swift
@@ -8,35 +8,33 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Home",
-            targets: ["Home"]),
-        .library(
-            name: "Login",
-            targets: ["Login"]),
-        .library(
-            name: "Profile",
-            targets: ["Profile"]),
+//        .library(name: "pocjournys", targets: ["pocjournys"]),
         
+        .library(name: "Home", targets: ["Home"]),
+        .library(name: "Login", targets: ["Login"]),
+        .library(name: "Profile", targets: ["Profile"]),
     ],
-    dependencies: [],
+    dependencies: [
+//        .package(path: "Sources/pocjourneys/Home"),
+//        .package(path: "Sources/pocjourneys/Login"),
+//        .package(path: "Sources/pocjourneys/Profile"),
+        
+        .package(name: "Core", url: "https://github.com/brunoccy/Core", .branch("main")),
+        .package(name: "AnalyticsInterfaces", url: "https://github.com/brunoccy/AnalyticsInterfaces", .branch("main")),
+        .package(name: "NetworkingInterfaces", url: "https://github.com/brunoccy/NetworkingInterfaces.git", .branch("main")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "Home", dependencies: [],
+//        .target(name: "pocjournys", sources: ["Home", "Login", "Profile"]),
+        
+        .target(name: "Home", dependencies: ["Core", "AnalyticsInterfaces", "NetworkingInterfaces"],
                 path: "Home/Sources/Home"),
-        .testTarget(name: "HomeTests", dependencies: ["Home"],
-                            path: "Home/Tests/HomeTests"),
-        
-        .target(name: "Login", dependencies: [],
+
+        .target(name: "Login", dependencies: ["Core", "AnalyticsInterfaces", "NetworkingInterfaces"],
                 path: "Login/Sources/Login"),
-        .testTarget(name: "LoginTests", dependencies: ["Login"],
-                            path: "Login/Tests/LoginTests"),
-        
-        .target(name: "Profile", dependencies: [],
+
+        .target(name: "Profile", dependencies: ["Core", "AnalyticsInterfaces", "NetworkingInterfaces"],
                 path: "Profile/Sources/Profile"),
-        .testTarget(name: "ProfileTests", dependencies: ["Profile"],
-                            path: "Profile/Tests/ProfileTests"),
-        
     ]
 )
